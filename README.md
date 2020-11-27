73e065434772f84c59cc8de77231d5b0ca46ca0d
Julia <juliariety@gmail.com>
2020-11-27 18:20:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# fix-factbase-search [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+Sometimes, [Factba.se][]'s search does not work when you're using an ad blocker.
+This extension fixes that.
+
+[factba.se]: https://factba.se
---
+++ src/content-script.js
@@ -0,0 +1,17 @@
+/**
+ * This extension works by adding a fake 'ga' (Google Analytics) function to
+ * the Factba.se search page.
+ *
+ * Because we can't directly access the 'window' object, which is where
+ * Factba.se expects to find the 'ga' object, we have to add it by using a
+ * dynamically created script tag. It's kind of hacky but hey, it works.
+ */
+const script = document.createElement('script')
+script.innerText = 'window.ga = () => {}'
+document.head.appendChild(script)
+
+/**
+ * And last but not least, we need to manually hide the
+ * "apologies, search may not work for you" error message.
+ */
+document.getElementById('filterline').remove()
